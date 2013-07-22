1017e2615bfbd52d095b6c5f2ee0097347d0c1c9
Luis Ribeiro <ribeiro.luis@hotmail.com>
2013-07-22 15:59:13 -0300

    programa

---
+++ ProgramaEstagio/ProgramaEstagio/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgramaEstagio
+{
+    class Cliente
+    {
+        List<Cliente> clienteLista = new List<Cliente>();
+
+        public Cliente(List<Cliente> _ClienteLista)
+        {
+            clienteLista = _ClienteLista;
+        }
+
+        public string CPF { get; set; }
+        public string Nome { get; set; }    
+
+
+
+
+    }
+}
---
+++ ProgramaEstagio/ProgramaEstagio/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgramaEstagio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
