847be8ea85e8ef847155b3159bfe7a8b8aa11c31
Luis Ribeiro <ribeiro.luis@hotmail.com>
2013-07-22 16:07:11 -0300

    classe cliente

--- ProgramaEstagio/ProgramaEstagio/Cliente.cs
+++ ProgramaEstagio/ProgramaEstagio/Cliente.cs
@@ -14,9 +14,84 @@ namespace ProgramaEstagio
             clienteLista = _ClienteLista;
         }
 
-        public string CPF { get; set; }
-        public string Nome { get; set; }    
+        public List<Cliente> IncluirCliente(Cliente _novoCliente)
+        {
+            try
+            {
+                clienteLista.Add(_novoCliente);
+                Console.WriteLine("Cliente Inserido com Sucesso..");
+                return clienteLista;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.ToString());
+                return clienteLista;
+            }
+        }
 
+        public List<Cliente> ExcluirCliente(string _cpfCliente)
+        {
+            try
+            {
+                int i = 0;
+                foreach (var item in clienteLista)
+                {
+                    if (item.CPF.Equals(_cpfCliente))
+                    {
+                        clienteLista.RemoveAt(i); 
+                        break;
+                    }
+                    else
+                        i++;
+
+                }
+                Console.WriteLine("Cliente Exluido com Sucesso..");
+                return clienteLista;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.ToString());
+                return clienteLista;
+            }
+        }
+
+        public List<Cliente> AlteraCliente(string _cpfCliente, Cliente _clienteAlterado)
+        {
+            try
+            {
+                int i = 0;
+                foreach (var item in clienteLista)
+                {
+                    if (item.CPF.Equals(_cpfCliente))
+                    {
+                        clienteLista[i] = _clienteAlterado;
+                        break;
+                    }
+                    else
+                        i++;
+
+                }
+                Console.WriteLine("Cliente Alterado com Sucesso..");
+                return clienteLista;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.ToString());
+                return clienteLista;
+            }
+        }
+
+        public string CPF { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string TelefoneFixo { get; set; }
+        public string TelefoneCelular { get; set; }
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
 
 
 
